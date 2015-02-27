571bc4db076068fa5bc625c03c4c78bfba795c8d
Sourin Sutradhar <sourin.sutradhar@olacabs.com>
2015-02-27 13:44:46 +0530

    Creating project

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c928df5a4e27849fec6f8cef6eb6f1ee1471a44aae958dbe9ca39bc252dcb05e27df9ff069dba4760a160aa7b7825ab44c2e0ab421d57a160beef349a96b967b'
