cf44c99f8c4616268cef2295f039b689a4662fbb
Sourin Sutradhar <sourin.sutradhar@olacabs.com>
2015-03-02 08:37:20 +0530

    Creating todo_app

---
+++ todo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = '5263765660a07164f0ba2c2b66af72c90777040dd3530497e07ed2845302cdf5aa0596e5f771263b5a3f6b853443ccb1a4dd07fc2a40de2d5a343ac935673aec'
